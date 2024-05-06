782f36b1f4ecfbed7249280616decce390afb40b
Bouba <boubacar.konate@outlook.fr>
2024-05-06 10:04:07 +0200

    configuration bdd

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -2,16 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getAuth, getReactNativePersistence  } from "firebase/auth";
 import { getFirestore  } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import Config from "./Config";
+// import {FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID } from '@env';
 
 // import AsyncStorage from '@react-native-async-storage/async-storage'; // Importez AsyncStorage
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZ4bfQXRF3n6XfkKIG9H1fBVoO-MqqqEg",
-  authDomain: "project-wecare.firebaseapp.com",
-  projectId: "project-wecare",
-  storageBucket: "project-wecare.appspot.com",
-  messagingSenderId: "772381402792",
-  appId: "1:772381402792:web:f8f647959db4a67dff449d"
+  apiKey: Config.FIREBASE_API_KEY,
+  authDomain: Config.FIREBASE_AUTH_DOMAIN,
+  projectId: Config.FIREBASE_PROJECT_ID,
+  storageBucket: Config.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Config.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Config.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
